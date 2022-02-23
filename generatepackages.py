a8967dae2b942fe907e2208de7aa6c4da7d59103
Stephen Eckels <stephen.eckels@fireeye.com>
2022-02-23 11:33:37 -0500

    merge pkg gen

--- generatepackages.py
+++ generatepackages.py
@@ -1,36 +1,37 @@
-import http.client
-import json
+# Python >= 3.6, tested with Python 3.9.5
+import requests
 
-# this generates stdpackages.go from the go github source tree
-def remove_prefix(text, prefix):
-    return text[text.startswith(prefix) and len(prefix):]
-
-conn = http.client.HTTPSConnection("api.github.com")
-payload = ''
-headers = {'User-Agent': 'python'}
-conn.request("GET", "/repos/golang/go/git/trees/master?recursive=0", payload, headers)
-res = conn.getresponse()
-txt = res.read().decode('utf-8')
-data = json.loads(txt)
+# https://docs.github.com/en/rest/reference/git#get-a-tree
+API_URL = "https://api.github.com/repos/golang/go/git/trees"
+DIR = "src"
+OUTPUT_FILE = "stdpackages.go"
+VAR_NAME = "standardPackages"
 
 
-with open('stdpackages.go', 'w') as f:
-    f.write("package main\nvar stdPkgs = []string{")
+def get_tree(tree_sha):
+    url = f"{API_URL}/{tree_sha}"
+    print(f"Getting {url}")
+    r = requests.get(url)
+    r.raise_for_status()
+    return r.json()
 
-    tree = data['tree']
-    for item in tree:
-        if item['type'] != 'tree':
-            continue
-        
-        path = item['path']
-        if not path.startswith('src'):
-            continue
-        
-        if path == "src":
-            continue
-        
-        path = remove_prefix(path, 'src/')
-        f.write("\"{}\",".format(path))
-        
-    f.write("\"\"}")
-       
+
+r = get_tree("master")
+for leaf in r["tree"]:
+    if leaf["path"] == DIR:
+        sha = leaf["sha"]
+        break
+
+r = get_tree(f"{sha}?recursive=1")
+
+if r["truncated"]:
+    raise RuntimeError("Too many paths, needed to fetch one sub-tree at a time")
+
+# Use list instead of set to keep order
+paths = [leaf["path"] for leaf in r["tree"] if leaf["type"] == "tree"]
+# paths in the following format: {"path1", "path2", ...}
+paths_str = '{"' + '", "'.join(paths) + '"}'
+
+print(f"Writing paths to {OUTPUT_FILE}")
+with open(OUTPUT_FILE, "w") as f:
+    f.write(f"package main\n\nvar {VAR_NAME} = []string{paths_str}")
