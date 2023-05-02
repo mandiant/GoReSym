bc768418c202238a2976006affa282d47e0a6621
Stephen Eckels <stephen.eckels@mandiant.com>
2023-05-02 12:55:47 -0400

    Optimize pattern scanner

--- objfile/scanner.go
+++ objfile/scanner.go
@@ -21,7 +21,7 @@ type SignatureMatch struct {
 }
 
 // TODO: Support more architectures in this mode - will involve big endian support for at least ppc64 (just one pointer read in the scanner here)
-var x64sig = signatureModuleDataInitx86_x64{21, 25, []byte("\x48\x8D\x05\x90\x90\x90\x90\x90\xE8\x90\x90\x90\x90\x48\x89\x44\x24\x90\x48\x8D\x0D\x90\x90\x90\x90\xEB\x0D")}
+var x64sig = signatureModuleDataInitx86_x64{21, 25, []byte("48 8D 05 ?? ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 89 44 24 ?? 48 8D 0D ?? ?? ?? ?? EB 0D")}
 
 // 0x0000000000061a74:  3C 80 00 2C    lis  r4, 0x2c       // moduledata
 // 0x0000000000061a78:  38 84 80 00    addi r4, r4, 0x8000  // moduledata ((0x2c << 16) + 0x8000)
@@ -29,31 +29,51 @@ var x64sig = signatureModuleDataInitx86_x64{21, 25, []byte("\x48\x8D\x05\x90\x90
 // 0x0000000000061a80:  E8 84 02 30    ld   r4, 0x230(r4)
 // 0x0000000000061a84:  7C 24 00 00    cmpd r4, r0
 // 0x0000000000061a88:  41 82 01 A8    beq  0x61c30
-var PPC_BE_sig = signatureModuleDataInitPPC{2, 6, []byte("\x90\x80\x00\x2C\x90\x90\x80\x00\x48\x90\x90\x08\x90\x90\x02\x30\x7C\x90\x00\x00\x41\x82\x90\x90")}
+var PPC_BE_sig = signatureModuleDataInitPPC{2, 6, []byte("3? 80 00 2C 3? ?? 80 00 48 ?? ?? 08 E? ?? 02 30 7C ?? 00 00 41 82 ?? ??")}
 
+func getPatternSize(signature []byte) int {
+	// c = 2 * b + (b - 1) . 2 chars per byte + b - 1 spaces between
+	return (len(signature) + 1) / 3
+}
+
+func getBits(x byte) byte {
+	if x >= '0' && x <= '9' {
+		return x - '0'
+	} else {
+		return (x & 0xDF) - 'A' + 0xa
+	}
+}
+
+// Pattern must have a space per byte, use ? as wildcard for nibbles, and be uppercase ascii text without the 0x or /x prefix
 func findPattern(data []byte, signature []byte, callback func(uint64) []SignatureMatch) []SignatureMatch {
 	var matches []SignatureMatch = make([]SignatureMatch, 0)
-	var sigIdx = uint64(0)
+	patternSize := getPatternSize(signature)
+	for i := range data {
+		sigIdx := 0
+		for sigIdx < patternSize {
+			sigPatIdx := sigIdx * 3
+			sigHi := getBits(signature[sigPatIdx:][0]) << 4
+			sigLo := getBits(signature[sigPatIdx:][1])
+			datByt := data[i+sigIdx:][0]
 
-	for idx := range data {
-		if uint64(len(data[idx:])) < uint64(len(signature))-sigIdx {
-			break
+			// check for ex: A?
+			if signature[sigPatIdx+1] == '?' {
+				sigLo = datByt & 0xF
+			}
+
+			if signature[sigPatIdx] == '?' {
+				sigHi = datByt & 0xF0
+			}
+
+			if datByt != (sigHi | sigLo) {
+				break
+			}
+
+			sigIdx += 1
 		}
 
-		if signature[sigIdx] == 0x90 {
-			// nop instruction is considered "wildcard"
-			sigIdx += 1
-		} else if signature[sigIdx] == data[idx] {
-			// Byte in signature is equal to position in data
-			sigIdx += 1
-		} else {
-			sigIdx = 0
-			continue
-		}
-
-		if sigIdx == uint64(len(signature))-1 {
-			matches = append(matches, callback(uint64(idx-len(signature)+2))...)
-			sigIdx = 0 // reset, scan rest of data for other matches
+		if sigIdx >= patternSize {
+			matches = append(matches, callback(uint64(i))...)
 		}
 	}
 	return matches
