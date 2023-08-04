91bbd8ba50971aee154625d4cff6131b993b567a
Willi Ballenthin <willi.ballenthin@gmail.com>
2023-08-04 13:33:23 +0000

    patterns: add some demonstrations of regexp group handling

--- go.mod
+++ go.mod
@@ -4,6 +4,12 @@ go 1.20
 
 require (
 	github.com/elliotchance/orderedmap v1.4.0
+	github.com/pkg/profile v1.7.0
 	golang.org/x/arch v0.0.0-20201008161808-52c3e6f60cff
 	rsc.io/binaryregexp v0.2.0
+)
+
+require (
+	github.com/felixge/fgprof v0.9.3 // indirect
+	github.com/google/pprof v0.0.0-20230728192033-2ba5b33183c6 // indirect
 )
--- objfile/patterns_test.go
+++ objfile/patterns_test.go
@@ -1,6 +1,7 @@
 package objfile
 
 import (
+	"fmt"
 	"testing"
 
 	"rsc.io/binaryregexp"
@@ -52,6 +53,138 @@ func TestBinaryRegex(t *testing.T) {
 		// byte 2 range is different
 		if !r.Match([]byte{0x48, 0x8D, 0x0F, 0x8F, 0xDA, 0x26, 0x00, 0xEB, 0x0D, 0x48, 0x8B, 0x89, 0x30, 0x02, 0x00, 0x00, 0x66, 0x0F, 0x1F, 0x44, 0x00, 0x00}) {
 			t.Errorf("failed to match data variant 2")
+		}
+	})
+}
+
+func compare(a [][]int, b [][]int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if len(a[i]) != len(b[i]) {
+			return false
+		}
+
+		for j := range a[i] {
+			if a[i][j] != b[i][j] {
+				return false
+			}
+		}
+
+	}
+
+	return true
+}
+
+// fetch the index of the subexp for the given regexp.
+//
+// this is called `(*Regexp) SubexpIndex` in recent Go,
+// but doesn't seem to be implemented in binaryregexp.
+// https://pkg.go.dev/regexp#Regexp.SubexpIndex
+func subexpIndex(re *binaryregexp.Regexp, name string) int {
+	for i, n := range re.SubexpNames() {
+		if n == name {
+			return i
+		}
+	}
+
+	return -1
+}
+
+// fetch the [start, end] pairs for the subexp with the given name in the given matches.
+func subexpIndexMatches(re *binaryregexp.Regexp, matches [][]int, name string) [][]int {
+	index := subexpIndex(re, name)
+
+	var ret [][]int
+	for _, match := range matches {
+
+		start := match[2*index]
+		end := match[2*index+1]
+
+		if start == -1 && end == -1 {
+			continue
+		}
+
+		ret = append(ret, []int{start, end})
+	}
+
+	fmt.Println(ret)
+	return ret
+}
+
+// basic demonstration of binaryregexp group handling.
+func TestRegexGrouping(t *testing.T) {
+	t.Run("documentation examples", func(t *testing.T) {
+		// from source: https://pkg.go.dev/regexp#Regexp.FindAllStringSubmatchIndex
+		re := binaryregexp.MustCompile(`a(x*)b`)
+
+		if !compare(re.FindAllStringSubmatchIndex("-ab-", -1), [][]int{{1, 3, 2, 2}}) {
+			t.Errorf("1")
+		}
+
+		if !compare(re.FindAllStringSubmatchIndex("-axxb-", -1), [][]int{{1, 5, 2, 4}}) {
+			t.Errorf("2")
+		}
+
+		if !compare(re.FindAllStringSubmatchIndex("-ab-axb-", -1), [][]int{{1, 3, 2, 2}, {4, 7, 5, 6}}) {
+			t.Errorf("3")
+		}
+
+		if !compare(re.FindAllStringSubmatchIndex("-axxb-ab-", -1), [][]int{{1, 5, 2, 4}, {6, 8, 7, 7}}) {
+			t.Errorf("4")
+		}
+
+		if !compare(re.FindAllStringSubmatchIndex("-foo-", -1), [][]int{}) {
+			t.Errorf("5")
+		}
+	})
+
+	t.Run("two groups", func(t *testing.T) {
+		re := binaryregexp.MustCompile(`((?P<xxx>a(x*)b)|(?P<yyy>c(y*)d))`)
+
+		fmt.Printf("aa: %v\n", re)
+		fmt.Printf("%q\n", re.SubexpNames())
+
+		if subexpIndex(re, "xxx") != 2 {
+			t.Errorf("xxx index")
+		}
+
+		if subexpIndex(re, "yyy") != 4 {
+			t.Errorf("yyy index")
+		}
+
+		if !compare(subexpIndexMatches(re, re.FindAllStringSubmatchIndex("--ab--", -1), "xxx"), [][]int{{2, 4}}) {
+			t.Errorf("1")
+		}
+
+		if !compare(subexpIndexMatches(re, re.FindAllStringSubmatchIndex("--axxb--", -1), "xxx"), [][]int{{2, 6}}) {
+			t.Errorf("2")
+		}
+
+		if !compare(subexpIndexMatches(re, re.FindAllStringSubmatchIndex("--ab--", -1), "yyy"), [][]int{}) {
+			t.Errorf("3: no matches")
+		}
+
+		if !compare(subexpIndexMatches(re, re.FindAllStringSubmatchIndex("--cd--", -1), "xxx"), [][]int{}) {
+			t.Errorf("4: no matches")
+		}
+
+		if !compare(subexpIndexMatches(re, re.FindAllStringSubmatchIndex("--cd--", -1), "yyy"), [][]int{{2, 4}}) {
+			t.Errorf("5")
+		}
+
+		if !compare(subexpIndexMatches(re, re.FindAllStringSubmatchIndex("--cyyd--", -1), "yyy"), [][]int{{2, 6}}) {
+			t.Errorf("6")
+		}
+
+		if !compare(subexpIndexMatches(re, re.FindAllStringSubmatchIndex("--abcd--", -1), "xxx"), [][]int{{2, 4}}) {
+			t.Errorf("7")
+		}
+
+		if !compare(subexpIndexMatches(re, re.FindAllStringSubmatchIndex("--abcd--", -1), "yyy"), [][]int{{4, 6}}) {
+			t.Errorf("8")
 		}
 	})
 }
