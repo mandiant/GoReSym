43c705325c525f8bcaefb94403035d438163bb3b
CatalinStratu <catalinstratu45@gmail.com>
2023-08-06 13:30:45 +0300

    Package Ioutil is deprecated

--- objfile/disasm.go
+++ objfile/disasm.go
@@ -13,7 +13,6 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -160,7 +159,7 @@ func (fc *FileCache) Line(filename string, line int) ([]byte, error) {
 	}
 
 	if e == nil {
-		content, err := ioutil.ReadFile(filename)
+		content, err := os.ReadFile(filename)
 		if err != nil {
 			return nil, err
 		}
