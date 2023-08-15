244799b4d4eedc5fb29bb856b5a42d3712f7c92f
Stephen Eckels <stevemk14ebr@gmail.com>
2023-08-15 11:39:41 -0400

    Add tests

--- build_test_files.sh
+++ build_test_files.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 trap "exit" INT
-sudo rm -rf $(pwd)/test
-versions=("1.19" "1.18" "1.17" "1.16" "1.15" "1.14" "1.13" "1.12" "1.11" "1.10" "1.9" "1.8" "1.7" "1.6" "1.5")
+sudo rm -rf $(pwd)/test/build
+sudo chmod -R 666 $(pwd)/test/
+versions=("1.21" "1.20" "1.19" "1.18" "1.17" "1.16" "1.15" "1.14" "1.13" "1.12" "1.11" "1.10" "1.9" "1.8" "1.7" "1.6" "1.5")
 for v in "${versions[@]}"
 do
     GO_TAG=$v
@@ -30,3 +31,6 @@ EOF
 done
 
 rm Dockerfile.test
+
+unzip $(pwd)/test/weirdbins.zip -d $(pwd)/test/
+sudo chmod -R 666 $(pwd)/test/
